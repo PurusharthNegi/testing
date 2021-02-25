1fd02cce21a6cec2743b4e4e0ffed333a4d5153f
HP <HP@Purusharth-HP-Pro>
2021-02-25 14:54:29 +0530

    Commit

---
+++ src/test/java/com/tail/Trail1.java
@@ -0,0 +1,12 @@
+package com.tail;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Trail1 {
+@Test
+public void test()
+{
+Reporter.log("Welcome Purusharth",true);	
+}
+}
